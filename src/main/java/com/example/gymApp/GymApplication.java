d77f5a9037c1314acc5243c9b0b5e3139872596b
romangalkin <garomait@gmail.com>
2024-11-19 10:16:35 +0400

    working code: mic 1 sends requests to mic 2 with feign

--- src/main/java/com/example/gymApp/GymApplication.java
+++ src/main/java/com/example/gymApp/GymApplication.java
@@ -2,10 +2,11 @@ package com.example.gymApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication(scanBasePackages = "com.example.gymApp")
-
+@EnableFeignClients(basePackages = "com.example.gymApp.feign")
 public class GymApplication {
 
   public static void main(String[] args) {
